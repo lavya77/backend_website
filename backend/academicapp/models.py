1d9b07fe955332676e3b1f43ddea2148a86c0217
Lavya <lavya203@gmail.com>
2025-06-26 13:08:53 +0530

    solves

--- backend/academicapp/models.py
+++ backend/academicapp/models.py
@@ -134,7 +134,7 @@ class Facultydirectory(models.Model):
         return self.title
 
 class FacultyMember(models.Model):
-    
+
     name = models.CharField(max_length=100)
     designation = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
@@ -145,7 +145,7 @@ class FacultyMember(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=20)
     image = models.ImageField(upload_to='faculty/')
-    icon_class = models.CharField(max_length=50),null=True,blank=True
+    icon_class = models.CharField(max_length=50,null=True,blank=True)
     background_color = models.CharField(max_length=50,null=True,blank=True)
     faculty_url = models.URLField()
 
