5bf10ce114d291780def89bdbc870325db45e761
shashaaankkkkk <shashankshekhar8534@gmail.com>
2025-06-26 07:33:55 +0000

    add

--- backend/landing_pageapp/models.py
+++ backend/landing_pageapp/models.py
@@ -107,8 +107,8 @@ class Campus_life(models.Model):
     description = models.TextField(null=True)
     image = models.ImageField(upload_to='realtedimg/')
     card_content = models.TextField(null=True)
-    name = models.CharField(max_length=20)
-    course = models.CharField(max_length=20)
+    name = models.CharField(max_length=20,null=True)
+    course = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.title
