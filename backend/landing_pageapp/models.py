bff0ac40995c536759cd9f64bb1e60e00aed0cac
Lavya <lavya203@gmail.com>
2025-06-26 13:14:51 +0530

    changes

--- backend/landing_pageapp/models.py
+++ backend/landing_pageapp/models.py
@@ -46,7 +46,9 @@ class Leadership(models.Model):
     url=models.CharField(max_length=255,null=True,blank=True)
 
     def __str__(self):
-        return self.name
+        return self.namepython manage.py makemigrations
+python manage.py migrate
+
 
 
 class GlanceStat(models.Model):
@@ -90,9 +92,9 @@ class Campus_gallery(models.Model):
 
 class Excellence_in_Education(models.Model):
     category_choices=[
-        {'centre of excellence',"Centers OF EXcellence"},
+        {'Centre Of Excellence',"Centers OF EXcellence"},
         ("Research Labs","Researc Labs"),
-        ("infrastructure","Infrastructure")
+        ("Infrastructure","Infrastructure")
     ]
     title = models.CharField(max_length=200)
     content_text = models.TextField()
