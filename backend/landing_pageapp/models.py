7ff03eff87c090cab22631d21f2e01c99023995c
Lavya <lavya203@gmail.com>
2025-06-26 13:17:31 +0530

    chnages

--- backend/landing_pageapp/models.py
+++ backend/landing_pageapp/models.py
@@ -46,10 +46,7 @@ class Leadership(models.Model):
     url=models.CharField(max_length=255,null=True,blank=True)
 
     def __str__(self):
-        return self.namepython manage.py makemigrations
-python manage.py migrate
-
-
+        return self.name
 
 class GlanceStat(models.Model):
     label = models.CharField(max_length=200)
