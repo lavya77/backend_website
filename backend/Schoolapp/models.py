2848f1ba6907f62920cc2f3cdc31f10018e434de
Lavya <lavya203@gmail.com>
2025-07-02 16:36:26 +0530

    chnages

--- backend/Schoolapp/models.py
+++ backend/Schoolapp/models.py
@@ -195,6 +195,12 @@ class faculty_herosection(models.Model):
     def __str__(self):
         return self.title
 
+class Departments_name(models.Model):
+    department_name=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.department_name        
+
 class faculty_department_herosection(models.Model):
     background_color=models.CharField(max_length=255,null=True,blank=True)
     title=models.CharField(max_length=255,null=True,blank=True)
@@ -214,7 +220,161 @@ class Faculty_profile(models.Model):
     email = models.EmailField(unique=True)
     phone_number=models.CharField(max_length=255,null=True,blank=True)
     profile_image=models.ImageField(upload_to='faculty_image',null=True,blank=True)
+    department=models.ForeignKey(Departments_name, on on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+class Board_studies(models.Model):
+    designation_choice=[
+        ('HoD','HoD'),
+        ('External Expert','External Expert'),
+        ('Member','Member'),
+        ('Chairperson','Chairperson')
+    ]
+    name=models.CharField(max_length=255,null=True,blank=True)
+    department=models.CharField(max_length=255,null=True,blank=True)
+    univeristy_name=models.CharField(max_length=255,null=True,blank=True)
+    phone_number=models.CharField(max_length=255,null=True,blank=True)
+    email=models.EmailField(max_length=255,null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
+class Ctaegory_lab(models.Model):
+    category_lab=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.category_lab
+
+class Laboratory_Excellence(models.Model):
+    equipped_choice=[
+        ("Fully Equipped","Fully Equipped"),
+        ("Empty","Empty")
+    ]
+    new=models.BooleanField()
+    status=models.CharField(max_length=255,null=True,blank=True)
+    equipped_status=models.CharField(max_length=255,null=True,blank=True,choices=equipped_choice)
+    category=models.ForeignKey(Ctaegory_lab,on_delete=models.CASCADE)
+    lab_name=models.CharField(max_length=255,null=True,blank=True)
+    faculty=models.CharField(max_length=255,null=True,blank=True)
+    Support=models.CharField(max_length=255,null=True,blank=True)
+    type_of=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.lab_name
+
+class conference_activity(models.Model):
+    title=models.CharField(max_length=255,null=True,blank=True)
+    duration=models.CharField(max_length=255,null=True,blank=True)
+    description=models.TextField(null=True)
+    url=models.CharField(max_length=255,null=True,blank=True)
+    icon_class=models.CharField(max_length=255,null=True,blank=True)
+    color=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Departments_herosection(models.Model):
+    background_color=models.CharField(max_length=255,null=True,blank=True)
+    background_image=models.ImageField(upload_to="schoool_department/",null=True,blank=True)
+    title=models.CharField(max_length=255,null=True,blank=True)
+    description=models.CharField(max_length=255,null=True,blank=True)
+    button1_text=models.CharField(max_length=255,null=True,blank=True)
+    button2_text=models.CharField(max_length=255,null=True,blank=True)
+    button1_url=models.CharField(max_length=255,null=True,blank=True)
+    button2_url=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+class deprtment_herosection_skills(model.Model):
+    icon=models.CharField(max_length=255,null=True,blank=True)
+    skill=models.CharField(max_length=255,null=True,blank=True)
+    description=models.TextField(null=True)
+
+    def __str__(self):
+        return self.skill
+
+class department_hod_message(models.Model):
+    profile_photo=models.CharField(max_length=255,null=True,blank=True)
+    name=models.CharField(max_length=255,null=True,blank=True)
+    designation=models.CharField(max_length=255,null=True,blank=True)
+    hod_message=models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class department_statistics(models.Model):
+    icon=models.CharField(max_length=255,null=True,blank=True)
+    value=models.CharField(max_length=255,null=True,blank=True)
+    label=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.label      
+class Academic_programs_department(model.Model):
+    program_name=models.CharField(max_length=255,null=True,blank=True)
+    description=models.TextField(null=True)
+    number_of_students=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.program_name
+class Curriculum_department(models.Model):
+    program=models.ForeignKey(Academic_programs_department,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255,null=True,blank=True)
+    category=models.CharField(max_length=255,null=True,blank=True)
+    order=models.PositiveIntegerField(default=0)
+
+    class meta:
+        ordering=['order']
+
+    def __str__(self):
+        return self.title
+class Research_excellence_departments(model.Model):
+    title=models.CharField(max_length=255,null=True,blank=True)
+    icon=models.CharField(max_length=255,null=True,blank=True)
+    description=models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
+
+class Research_excellence_departments_projects(models.Model):
+    Research_topic=models.ForeignKey(Research_excellence_departments,on_delete=models.CASCADE)
+    project=models.CharField(max_length=255,null=True,blank=True)
+    order=models.PositiveIntegerField(default=0)
+
+    class meta:
+        ordering=['order']
+    def __str__(self):
+        return self.project 
+
+class Research_excellence_departments_stats(models.Model):
+    icon=models.CharField(max_length=255,null=True,blank=True)
+    label=models.CharField(max_length=255,null=True,blank=True)
+    color=models.CharField(max_length=255,null=True,blank=True)
+    
+
+    def __str__(self):
+        return self.label
+class Expertise_skills_student_achiever(models.Model):
+    skill=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.skill
+
+class Studnet_achievers_department(models.Model):
+    profile=models.ImageField(upload_to='student_achiver/',null=True,blank=True)
+    name=models.CharField(max_length=255,null=True,blank=True)
+    course=models.CharField(max_length=255,null=True,blank=True)
+    highlights=models.CharField(max_length=255,null=True,blank=True)
+    expertise=models.ManyToManyField(Expertise_skills_student_achiever)
+
+    def __str__(self):
+        return self.profile
+
+class Our_achievements_departemnts(models.Model):
+    icon=models.CharField(max_length=255,null=True,blank=True)
+    title=models.CharField(max_length=255,null=True,blank=True)
+    description=models.CharField(max_length=255,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
 
